3db20948877e6440e83b94f82982fdb8865ac3c0
unknown <jackcameron.jobs@gmail.com>
2025-08-07 14:44:52 +1000

    added autorag example request

--- llm_req_example.py
+++ llm_req_example.py
@@ -9,8 +9,10 @@ load_dotenv()
 api_key = os.getenv('API_KEY')
 account_id = os.getenv('ACCOUNT_ID')
 ai_gateway = os.getenv('AI_GATEWAY')
+autorag_name = os.getenv('AUTORAG_NAME')
+autorag_api_key = os.getenv('AUTORAG_API_KEY')
 
-# Construct the URL
+# regular model search
 url = f"https://gateway.ai.cloudflare.com/v1/{account_id}/{ai_gateway}/workers-ai/@cf/meta/llama-3.1-8b-instruct"
 
 headers = {
@@ -31,9 +33,25 @@ data = {
     ]
 }
 
+#auto rag search
+url2 = f"https://api.cloudflare.com/client/v4/accounts/{account_id}/autorag/rags/{autorag_name}/ai-search"
+
+headers2 = {
+    "Authorization": f"Bearer {autorag_api_key}",
+    "Content-Type": "application/json"
+}
+
+data2 = {
+    "query": "What is Dota 2?"
+}
+
+
 try:
     response = requests.post(url, headers=headers, json=data)
     response.raise_for_status()
     print(response.json())
+    response2 = requests.post(url2, headers=headers2, json=data2)
+    response2.raise_for_status()
+    print(response2.json())
 except requests.exceptions.RequestException as e:
     print(f"Error: {e}")
